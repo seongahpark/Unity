43da0863d1df8ab54f796a83125145a42ea07198
seongahpark <sksmsekekek21@naver.com>
2021-11-23 17:47:05 +0900

    animation pivot 조정 완료, camera setting 완료, 욕망의헌신 pivot 조정 완료

---
+++ Project_Maplestory/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    private float speed = 3.0f;
+    private float jumpForce = 300f;
+    private float checkRadius = 0.35f;
+
+    private bool isTouchRight = false;
+    private bool isTouchLeft = false;
+    private bool isGround = false;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * Time.deltaTime * speed;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+}
